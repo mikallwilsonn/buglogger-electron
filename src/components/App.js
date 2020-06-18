7f88f3bff2a57e678035c2fa8ddae7d481cd2e75
Michael Wilson <mikallwilsonn@gmail.com>
2020-06-18 09:54:19 -0400

    Completed 'Log State & Table'

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,74 @@
 // ----
 // Dependencies
-import React from 'react'
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+
+
+// ----
+// Child Components
+import LogItem from './LogItem';
 
 
 // ----
 // App functional component
 const App = () => {
-	return (
-		<div className='app'>
-			<h1>
-				React Electron Boilerplate
-			</h1>
+	const [ logs, setLogs ] = useState([
+		{
+			id: 1,
+			text: 'This is log one',
+			priority: 'low',
+			user: 'Bob',
+			created: new Date().toString()
+		},
+		{
+			id: 2,
+			text: 'A second log',
+			priority: 'moderate',
+			user: 'Jim',
+			created: new Date().toString()
+		},
+		{
+			id: 3,
+			text: 'Here, this is a third log to show',
+			priority: 'high',
+			user: 'Jane',
+			created: new Date().toString()
+		},
+	]);
 
-			<p>
-				This is a simple boilerplate for using React with Electron
-			</p>
-		</div>
+	return (
+		<Container className='app'>
+			<Table>
+				<thead>
+					<tr>
+						<th>
+							Priority
+						</th>
+
+						<th>
+							Log Text
+						</th>
+
+						<th>
+							User
+						</th>
+
+						<th>
+							Created Date
+						</th>
+
+						<th></th>
+					</tr>
+				</thead>
+
+				<tbody>
+					{ logs.map(( log ) => {
+						return <LogItem key={ log.id } />
+					})}
+				</tbody>
+			</Table>
+		</Container>
 	)
 }
 
---
+++ src/components/LogItem.js
@@ -0,0 +1,23 @@
+// ----
+// Dependencies
+import React from 'react';
+
+
+// ----
+// LogItem functional component
+const LogItem = () => {
+    return (
+        <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td>5</td>
+        </tr>
+    );
+}
+
+
+// ----
+// Export
+export default LogItem;
